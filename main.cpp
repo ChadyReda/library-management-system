7b6b1e9e41b1b5ebcaf8d9981e84d35eebbb6c9b
chady <chadyxreda@gmail.com>
2024-10-27 19:34:29 +0100

    admin and book classes

--- main.cpp
+++ main.cpp
@@ -3,6 +3,36 @@
 
 using namespace std;
 
+class Book {
+    private:
+        string title;
+        string author;
+        bool available;
+    public:
+        string getTitle() const {
+            return title;
+        }
+        string getAuthor() const {
+            return author;
+        }
+        bool isAvailable() const {
+            return available;
+        }
+        void setAvailability(const bool& availability) {
+            available = availability;
+        }
+        void getDetails() const {
+            cout << "Title: " << title 
+                 << " Author: " << author 
+                 << " Available: " << available
+                 << endl; 
+        }
+        void setTitle(const string& t) {title = t;}
+        void setAuthor(const string& a) {author = a;}
+};
+
+
+
 class Person {
     protected: 
         string name;
@@ -26,6 +56,37 @@ class Person {
         }
 
 };
+
+class Admin : public Person {
+    public:
+        Admin(string name, int id): Person(name, id) {}
+        void manageBooks () {
+            cout << "Managing books ..." << endl;
+        }
+        void manageMembers () {
+            cout << "Managing members ..." << endl;
+        }
+
+};
+
+class Member : public Person {
+    public:
+        Member (string name, int id): Person (name, id) {}
+        void borrowBook(Book& book) {
+            if (book.isAvailable()) {
+                cout << name << " borrowed the book : " << book.getTitle() << endl;
+                book.setAvailability(false);
+            }     
+            cout << "Book is currently unavailable!" << endl;
+        }
+        void returnBook(Book& book) {
+            book.setAvailability(true);
+            cout << name << " returned the book: " << book.getTitle() << endl;
+        }
+};
+
+
+
 
 
 int main() {
